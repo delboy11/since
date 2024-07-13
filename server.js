76e3c04d783ef9ab8c386316a942e64c77d295f2
delboy11 <dbswnstj765@naver.com>
2024-07-13 23:02:39 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 
 app.listen(8080,function(){
- 
+    
 })
 
-/*app.get('/sexy',function(요청 ,응답){
-    응답.send('하이')
-})*/
+app.get('/sexy',function(요청 ,응답){
+    응답.send('엄준식')
+})
 
 app.get('/',function(요청 ,응답){
     응답.sendFile(__dirname + '/index.html')
-    
 })
